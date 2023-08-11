8980aec5729fa842ab9b1bc3b797b11d0d3c5a7b
Pedro Meneghel <pedromeneghel@gmail.com>
2023-08-11 17:29:42 -0300

    feat: adiciona a screen AddMeal

--- src/screens/AddMeal/index.tsx
+++ src/screens/AddMeal/index.tsx
@@ -1,9 +1,94 @@
-import { Text, View } from 'react-native';
+import { Button } from '@components/Button';
+import { Header } from '@components/Header';
+import { Input } from '@components/Input';
+import { useState } from 'react';
+import {
+  Container,
+  Container50,
+  Content,
+  Form,
+  Label,
+  OneColumnContainer,
+  TwoColumnContainer,
+} from './styles';
+import { Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { mealCreate } from '@storage/meals/mealCreate';
+import { MealStorageDTO } from '@storage/meals/MealStorageDTO';
+import { AppError } from '@utils/AppError';
 
 export function AddMeal() {
+  const navigation = useNavigation();
+  const [meal, setMeal] = useState('');
+  const [description, setDescription] = useState('');
+  const [date, setDate] = useState('');
+  const [hour, setHour] = useState('');
+  const [isInDiet, setIsInDiet] = useState(true);
+
+  async function handleNewMeal() {
+    try {
+      if (meal.trim().length <= 2) {
+        return Alert.alert('Ops', 'Informe o nome da refeição.');
+      }
+
+      await mealCreate({
+        date: '01/01/2023',
+        meal,
+        description,
+        hour,
+        isInDiet,
+      });
+
+      // navigation.navigate('players', { group });
+      Alert.alert('Sucesso!', 'Refeição criada com sucesso!');
+    } catch (error: any) {
+      if (error instanceof AppError) {
+        Alert.alert('Ops', error.message);
+      } else {
+        Alert.alert(
+          'Ops',
+          'Algo deu errado, por favor tente novamente mais tarde.',
+        );
+      }
+    }
+    console.log(meal, description, date, hour, isInDiet);
+  }
+
   return (
-    <View>
-      <Text>Criar refeição</Text>
-    </View>
+    <Container>
+      <Header type="SECONDARY" title="Nova refeição" showBackButton />
+      <Content>
+        <Form>
+          <OneColumnContainer>
+            <Label>Nome</Label>
+            <Input onChangeText={setMeal} />
+          </OneColumnContainer>
+          <OneColumnContainer>
+            <Label>Descrição</Label>
+            <Input
+              onChangeText={setDescription}
+              multiline={true}
+              numberOfLines={10}
+              style={{ height: 200, textAlignVertical: 'top' }}
+            />
+          </OneColumnContainer>
+          <TwoColumnContainer>
+            <Container50>
+              <Label>Data</Label>
+              <Input onChangeText={setDate} />
+            </Container50>
+            <Container50>
+              <Label>Hora</Label>
+              <Input onChangeText={setHour} />
+            </Container50>
+          </TwoColumnContainer>
+        </Form>
+        <Button
+          color="BASE"
+          title="Cadastrar refeição"
+          onPress={handleNewMeal}
+        />
+      </Content>
+    </Container>
   );
 }
---
+++ src/screens/AddMeal/styles.ts
@@ -0,0 +1,48 @@
+import styled, { css } from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.BASE.WHITE};
+`;
+
+export const Content = styled.ScrollView`
+  flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.BASE.WHITE};
+  border-radius: 20px;
+  padding: 40px 24px 24px 24px;
+  margin-top: -20px;
+`;
+
+export const Form = styled.View`
+  flex: 1;
+`;
+
+export const OneColumnContainer = styled.View`
+  width: 100%;
+  min-height: 70px;
+  margin-bottom: 24px;
+`;
+
+export const TwoColumnContainer = styled.View`
+  width: 100%;
+  min-height: 70px;
+  margin-bottom: 24px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const Container50 = styled.View`
+  width: 47%;
+  min-height: 70px;
+  margin-bottom: 24px;
+`;
+
+export const Label = styled.Text`
+  width: 100%;
+  margin-bottom: 5px;
+  ${({ theme }) => css`
+    color: ${theme.COLORS.BASE.GRAY_2};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-size: ${theme.FONT_SIZE.SM}px;
+  `}
+`;
