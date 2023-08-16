efad3cf3d4a749c2a040114e67be41e05e9ab19d
Pedro Meneghel <pedromeneghel@gmail.com>
2023-08-16 07:38:47 -0300

    feat: adiciona a tela de estatísticas

--- src/screens/Statistics/index.tsx
+++ src/screens/Statistics/index.tsx
@@ -1,5 +1,46 @@
 import { Text } from 'react-native';
+import {
+  Container,
+  Content,
+  GeneralStats,
+  MealsInDietContainer,
+  MealsOutDietContainer,
+  StatsIndicator,
+  StatsText,
+  Title,
+  TwoColumnsContainer,
+} from './styles';
+import { HeaderStatistics } from '@components/HeaderStatistics';
 
 export function Statistics() {
-  return <Text>Estatísticas</Text>;
+  return (
+    <Container>
+      <HeaderStatistics />
+      <Content>
+        <Title>Estatísticas gerais</Title>
+
+        <GeneralStats>
+          <StatsIndicator>22</StatsIndicator>
+          <StatsText>melhor sequência de pratos dentro da dieta</StatsText>
+        </GeneralStats>
+
+        <GeneralStats>
+          <StatsIndicator>109</StatsIndicator>
+          <StatsText>refeições registradas</StatsText>
+        </GeneralStats>
+
+        <TwoColumnsContainer>
+          <MealsInDietContainer>
+            <StatsIndicator>99</StatsIndicator>
+            <StatsText>refeições dentro da dieta</StatsText>
+          </MealsInDietContainer>
+
+          <MealsOutDietContainer>
+            <StatsIndicator>10</StatsIndicator>
+            <StatsText>refeições fora da dieta</StatsText>
+          </MealsOutDietContainer>
+        </TwoColumnsContainer>
+      </Content>
+    </Container>
+  );
 }
---
+++ src/screens/Statistics/styles.ts
@@ -0,0 +1,84 @@
+import styled, { css } from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.BASE.GRAY_7};
+`;
+
+export const Content = styled.ScrollView`
+  flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.BASE.GRAY_7};
+  border-radius: 20px;
+  padding: 40px 24px 24px 24px;
+  margin-top: -20px;
+`;
+
+export const Title = styled.Text`
+  flex: 1;
+  text-align: center;
+  margin-bottom: 23px;
+  ${({ theme }) => css`
+    color: ${theme.COLORS.BASE.GRAY_1};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-size: ${theme.FONT_SIZE.SM}px;
+  `}
+`;
+
+export const GeneralStats = styled.View`
+  width: 100%;
+  min-height: 89px;
+  border-radius: 8px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.COLORS.BASE.GRAY_6};
+  margin-bottom: 12px;
+`;
+
+export const TwoColumnsContainer = styled.View`
+  width: 100%;
+  min-height: 107px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const MealsInDietContainer = styled.View`
+  width: 48.5%;
+  min-height: 107px;
+  border-radius: 8px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.COLORS.PRODUCT.GREEN_LIGHT};
+  margin-bottom: 12px;
+  padding: 15px;
+`;
+
+export const MealsOutDietContainer = styled.View`
+  width: 48.5%;
+  min-height: 107px;
+  border-radius: 8px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.COLORS.PRODUCT.READ_LIGHT};
+  margin-bottom: 12px;
+  padding: 15px;
+`;
+
+export const StatsIndicator = styled.Text`
+  margin-bottom: 8px;
+  ${({ theme }) => css`
+    color: ${theme.COLORS.BASE.GRAY_1};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-size: ${theme.FONT_SIZE.XL}px;
+    line-height: ${theme.LINE_HEIGHT * theme.FONT_SIZE.XL / 100}px;
+  `}
+`;
+
+export const StatsText = styled.Text`
+  text-align: center;
+  ${({ theme }) => css`
+    color: ${theme.COLORS.BASE.GRAY_2};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    font-size: ${theme.FONT_SIZE.SM}px;
+    line-height: ${theme.LINE_HEIGHT * theme.FONT_SIZE.SM / 100}px;
+  `}
+`;
