32e2a361a331782db992d4304e5ae7b24daa7c79
Pedro Meneghel <pedromeneghel@gmail.com>
2023-08-22 07:52:43 -0300

    feat: adiciona função de remover refeição por id

---
+++ src/storage/meals/mealRemoveById.ts
@@ -0,0 +1,15 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { MEALS_COLLECTION } from '@storage/storageConfig';
+import { mealsGetAll } from './mealsGetAll';
+
+export async function mealRemoveById(mealId: string): Promise<void> {
+  try {
+    const storedMeals = await mealsGetAll();
+    const filteredMeals = storedMeals.filter(meal => meal.id !== mealId);
+    const storage = JSON.stringify([...filteredMeals]);
+
+    await AsyncStorage.setItem(MEALS_COLLECTION, storage);
+  } catch (error) {
+    throw error;
+  }
+}
